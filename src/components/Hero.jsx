9fb7ed20b65010b368fe53ab093e513b4cbcec81
rtaranov17 <113447036+rtaranov17@users.noreply.github.com>
2023-11-27 23:36:03 +0000

    Updated Hero - We are Hiring

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,6 +13,15 @@ export function Hero() {
   
   return (
     <Container className="pb-16 pt-20 text-center lg:pt-32" id="#home">
+      <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              We are hiring.{' '}
+              <a href="/careers" className="font-semibold text-blue-600">
+                <span className="absolute inset-0" aria-hidden="true" />
+                Learn more <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
       
         <span className="relative whitespace-nowrap text-blue-600">
@@ -32,18 +41,9 @@ export function Hero() {
       End-to-end marketing and growth offerings, leveraging our skilled team&apos;s forward-thinking techniques and extensive industry insights to enable businesses to prosper in competitive markets.
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
-        <Button href="#solutions">Learn More</Button>
-        <Button
-          href="/careers"
-          variant="outline"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="h-5 w-5 flex-none fill-blue-600 group-active:fill-current">
-  <path fill-rule="evenodd" d="M6 3.75A2.75 2.75 0 018.75 1h2.5A2.75 2.75 0 0114 3.75v.443c.572.055 1.14.122 1.706.2C17.053 4.582 18 5.75 18 7.07v3.469c0 1.126-.694 2.191-1.83 2.54-1.952.599-4.024.921-6.17.921s-4.219-.322-6.17-.921C2.694 12.73 2 11.665 2 10.539V7.07c0-1.321.947-2.489 2.294-2.676A41.047 41.047 0 016 4.193V3.75zm6.5 0v.325a41.622 41.622 0 00-5 0V3.75c0-.69.56-1.25 1.25-1.25h2.5c.69 0 1.25.56 1.25 1.25zM10 10a1 1 0 00-1 1v.01a1 1 0 001 1h.01a1 1 0 001-1V11a1 1 0 00-1-1H10z" clip-rule="evenodd" />
-  <path d="M3 15.055v-.684c.126.053.255.1.39.142 2.092.642 4.313.987 6.61.987 2.297 0 4.518-.345 6.61-.987.135-.041.264-.089.39-.142v.684c0 1.347-.985 2.53-2.363 2.686a41.454 41.454 0 01-9.274 0C3.985 17.585 3 16.402 3 15.055z" />
-</svg>
-
-          <span className="ml-3">Careers at WinTech</span>
-        </Button>
+        <Button href="#solutions" >
+          Learn More <span aria-hidden="true"> →</span>
+          </Button>
       </div>
       <div className="mt-36 lg:mt-44">
         <p className="font-display text-base text-slate-900">
